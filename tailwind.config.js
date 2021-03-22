89412061fc7f1b3a9b1879e7625c3087807c49ef
Cardoso-topdev <Oscar@cardoso-devstar.com>
2021-03-21 19:42:59 -0600

    fixed tailwind problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: {
-    preserveHtmlElements: false,
-    content: ['./src/**/*.html'],
-  },
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
